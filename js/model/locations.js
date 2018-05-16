d36b6aec96d1469bc4300490baef8886260e4560
Anna <polozova.anastasi@gmail.com>
2018-05-16 18:46:04 +0300

    change locations

---
+++ js/model/locations.js
@@ -0,0 +1,17 @@
+// Default locations
+const defaultLocations = [
+    { title: 'Charleys Philly Steaks', location: {lat: 55.856402, lng: 37.4135128} },
+    { title: 'lepim i varim', location: {lat: 55.7704948, lng: 37.6153096} },
+    { title: 'Molon Lave', location: {lat: 55.7705227, lng: 37.5791593} },
+    { title: 'Pinzeria by Bontempi', location: {lat: 55.7460581, lng: 37.6001191} },
+    { title: "Buffalo's - Wings and Nuggets", location: {lat: 55.7519279, lng: 37.5850309} },
+    { title: 'Streat', location: {lat: 55.7092196, lng: 37.6506823} },
+    { title: 'Black Thai', location: {lat: 55.767271, lng: 37.605504} },    
+    { title: 'Cevicheria', location: {lat: 55.738734, lng: 37.603044} },    
+    { title: 'White Rabbit', location: {lat: 55.7473124, lng: 37.5797342} },    
+    { title: 'Wine Room Gallery Gourme', location: {lat: 55.701773, lng: 37.49678} },
+    { title: "Uilliam's", location: {lat: 55.7624551, lng: 37.5932802} },    
+    { title: 'Boston Seafood & Bar', location: {lat: 55.7539499, lng: 37.5807561} },
+    { title: "Let’s Restaurant & Bar", location: {lat: 55.772846, lng: 37.627632} },
+    { title: 'Camorra Pizza e Birra', location: {lat: 55.7550252, lng: 37.6349029} }
+];
